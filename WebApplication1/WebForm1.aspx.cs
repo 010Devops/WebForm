5ec1918b4244ae6d3b4fa57d5baae325561f7c7d
010Devops <111432270+010Devops@users.noreply.github.com>
2022-10-28 17:07:11 +0530

    Update WebForm1.aspx.cs

--- WebApplication1/WebForm1.aspx.cs
+++ WebApplication1/WebForm1.aspx.cs
@@ -10,7 +10,7 @@ namespace WebApplication1
 {
     public partial class WebForm1 : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
+        protected void SubmitButton_Click(object sender, EventArgs e)
         {
             userInput.Text = UserName.Text; 
         }
